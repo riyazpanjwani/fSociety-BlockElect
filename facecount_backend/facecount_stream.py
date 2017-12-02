4e8fdb52ad763436fb8ea1d3622dc175945bab33
Utkarsh Gupta <utkarsh.gupta550@gmail.com>
2017-12-02 14:23:49 +0530

    Asynchronous notification

--- facecount_backend/facecount_stream.py
+++ facecount_backend/facecount_stream.py
@@ -2,9 +2,9 @@ import cv2
 
 class CaptureVideo(object):
     def __init__(self):
-        self.face_count = 0
+        pass
 
-    def live_stream_counter(self):
+    def live_stream_counter(self, share_inst):
         faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
         video_capture_instance = cv2.VideoCapture(0)
@@ -15,13 +15,12 @@ class CaptureVideo(object):
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
             faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5,
-                    minSize=(30, 30), flags=cv2.CV_HAAR_SCALE_IMAGE)
+                    minSize=(30, 30), flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
 
-            self.face_count = len(faces)
-            print(len(faces))
-            cv2.imshow('Video', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            if(len(faces) > 1):
+                share_inst.set(2)
                 break
-
+            else:
+                share_inst.set(1)
         cap.release()
         cv2.destroyAllWindows()
